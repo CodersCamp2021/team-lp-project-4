d69c4714ac885b0d4a4564a22d56a81a4de85540
pastepi <pawel.st123@gmail.com>
2022-04-03 22:25:53 +0200

    Formatting fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import ReactDOM from 'react-dom';
+import App from './App';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <HashRouter>
     <Routes>
-      <Route path='/' element={<App />} />
+      <Route path="/" element={<App />} />
     </Routes>
   </HashRouter>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
