c98663728525fcc6d69d0ade35fbc923d3362e15
pastepi <pawel.st123@gmail.com>
2022-04-03 22:22:38 +0200

    Pre-commit check

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
   return (
-  <QueryClientProvider client={queryClient}>
-    <div>Hello, World!</div>
-  </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <div>Hello, World!</div>
+    </QueryClientProvider>
   );
 }
 
