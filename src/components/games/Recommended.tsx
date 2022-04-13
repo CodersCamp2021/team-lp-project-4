a700e9ff9a78aabc8eed79476a9bf4280adb809a
pastepi <p.stepien@livechat.com>
2022-04-14 01:43:54 +0200

    Adjusted animations based on viewport width

--- src/components/games/Recommended.tsx
+++ src/components/games/Recommended.tsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 
 import useGamesStyles from '../../hooks/use-games-styles';
 import GameCard from './GameCard';
+import { useMediaQuery } from '@mantine/hooks';
 
 const placeholderData = [
   {
@@ -77,14 +78,21 @@ const placeholderData = [
   },
 ];
 
-const variants = {
+const variantsLarge = {
   current: { scale: 1.25, originY: 1 },
   adjacent: { scale: 1.1, originY: 1 },
+};
+
+const variantsSmall = {
+  current: { scale: 1.13, originY: 1 },
+  adjacent: { scale: 1.05, originY: 1 },
 };
 
 const Recommended = () => {
   const [hoveredCard, setHoveredCard] = useState<number | null>(null);
   const { classes } = useGamesStyles();
+  const smallScreen = useMediaQuery('(max-width: 870px)');
+  const mediumScreen = useMediaQuery('(max-width: 1250px)');
 
   return (
     <Box>
@@ -101,13 +109,15 @@ const Recommended = () => {
             animate={
               hoveredCard === index
                 ? 'current'
-                : hoveredCard === index - 1
+                : smallScreen // if screen === small, no animation for adjacent cards
+                ? undefined
+                : hoveredCard === index + 1 // otherwise, check for adjacent cards and add animation
                 ? 'adjacent'
-                : hoveredCard === index + 1
+                : hoveredCard === index - 1
                 ? 'adjacent'
                 : undefined
             }
-            variants={variants}
+            variants={mediumScreen ? variantsSmall : variantsLarge}
           >
             <GameCard
               game={game}
