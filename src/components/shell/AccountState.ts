09031ddc867478587b802246fefe986c9025d168
Jakub Czerwinski <kubaczerwinski77@gmail.com>
2022-04-25 15:35:45 +0200

    password change added

--- src/components/shell/AccountState.ts
+++ src/components/shell/AccountState.ts
@@ -1,11 +1,97 @@
+import { showNotification } from '@mantine/notifications';
+import { Response } from '../../hooks/fetchData';
+
 export const handleLogout = (fn: () => void) => {
-  fetch(`http://localhost:3001/user/logout`, {
+  fetch('http://localhost:3001/user/logout', {
     method: 'POST',
     credentials: 'include',
   })
     .then((res) => res.json())
     .then(() => {
       fn();
+    })
+    .catch((err) => console.error(err));
+};
+
+export const handleChangeEmail = ({
+  userId,
+  email,
+  password,
+}: {
+  userId: string | undefined;
+  email: string;
+  password: string;
+}) => {
+  fetch(`http://localhost:3001/user/changeEmail/${userId ? userId : ''}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+    mode: 'cors',
+    credentials: 'include',
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((res: Response) => {
+      if (res.message) {
+        showNotification({
+          title: 'Success!',
+          message: res.message,
+          color: 'teal',
+        });
+      } else {
+        showNotification({
+          title: 'Something went wrong!',
+          message: res.error,
+          color: 'red',
+        });
+      }
+    })
+    .catch((err) => console.error(err));
+};
+
+export const handleChangePassword = ({
+  userId,
+  password,
+  newPassword,
+  passwordConfirmation,
+}: {
+  userId: string | undefined;
+  password: string;
+  newPassword: string;
+  passwordConfirmation: string;
+}) => {
+  fetch(`http://localhost:3001/user/changePassword/${userId ? userId : ''}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      password,
+      newPassword,
+      passwordConfirmation,
+    }),
+    mode: 'cors',
+    credentials: 'include',
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((res: Response) => {
+      if (res.message) {
+        showNotification({
+          title: 'Success!',
+          message: res.message,
+          color: 'teal',
+        });
+      } else {
+        showNotification({
+          title: 'Something went wrong!',
+          message: res.error,
+          color: 'red',
+        });
+      }
     })
     .catch((err) => console.error(err));
 };
--- src/components/shell/AppHeader.tsx
+++ src/components/shell/AppHeader.tsx
@@ -7,22 +7,61 @@ import {
   Button,
   Box,
   Menu,
+  Modal,
+  Space,
 } from '@mantine/core';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserButton } from './UserButton';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../AuthContext';
 import { BiLogOut } from 'react-icons/bi';
 import { RiLockPasswordLine } from 'react-icons/ri';
 import { MdOutlineAlternateEmail } from 'react-icons/md';
-import { handleLogout } from './AccountState';
+import {
+  handleChangeEmail,
+  handleChangePassword,
+  handleLogout,
+} from './AccountState';
+import * as Yup from 'yup';
+import { useForm, yupResolver } from '@mantine/form';
+import Input from '../sign-in/Input';
+import PasswordInput from '../sign-up/PasswordInput';
 
 type AppHeaderProps = {
   opened: boolean;
   classes: Record<'header' | 'navbar' | 'links', string>;
   setOpenedCallback: React.Dispatch<React.SetStateAction<boolean>>;
 };
+
+const emailChangeSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email'),
+  password: Yup.string(),
+});
+
+const passwordChangeSchema = Yup.object().shape({
+  password: Yup.string(),
+  newPassword: Yup.string(),
+  passwordConfirmation: Yup.string(),
+});
+
 const AppHeader = ({ opened, classes, setOpenedCallback }: AppHeaderProps) => {
+  const emailForm = useForm({
+    schema: yupResolver(emailChangeSchema),
+    initialValues: {
+      email: '',
+      password: '',
+    },
+  });
+  const passwordForm = useForm({
+    schema: yupResolver(passwordChangeSchema),
+    initialValues: {
+      password: '',
+      newPassword: '',
+      passwordConfirmation: '',
+    },
+  });
+  const [emailModalOpened, setEmailModalOpened] = useState(false);
+  const [passwordModalOpened, setPasswordModalOpened] = useState(false);
   const auth = useContext(AuthContext);
   const navigate = useNavigate();
   return (
@@ -97,13 +136,13 @@ const AppHeader = ({ opened, classes, setOpenedCallback }: AppHeaderProps) => {
                   <Menu.Label>Account settings</Menu.Label>
                   <Menu.Item
                     icon={<MdOutlineAlternateEmail size={14} />}
-                    onClick={() => console.log('Changing email...')}
+                    onClick={() => setEmailModalOpened(true)}
                   >
                     Change email
                   </Menu.Item>
                   <Menu.Item
                     icon={<RiLockPasswordLine size={14} />}
-                    onClick={() => console.log('Changing password...')}
+                    onClick={() => setPasswordModalOpened(true)}
                   >
                     Change password
                   </Menu.Item>
@@ -152,6 +191,76 @@ const AppHeader = ({ opened, classes, setOpenedCallback }: AppHeaderProps) => {
           </Group>
         </Box>
       </Box>
+      {
+        //email modal
+      }
+      <Modal
+        opened={emailModalOpened}
+        onClose={() => setEmailModalOpened(false)}
+      >
+        <Box sx={{ maxWidth: 340 }} mx="auto">
+          <form
+            onSubmit={emailForm.onSubmit((values) => {
+              handleChangeEmail({ ...values, userId: auth?.userInfo?.id });
+              setEmailModalOpened(false);
+            })}
+          >
+            <Input
+              label="New Email"
+              placeholder="example@mail.com"
+              formProps={emailForm.getInputProps('email')}
+            />
+            <Space h="lg" />
+            <PasswordInput
+              label="Password"
+              formProps={emailForm.getInputProps('password')}
+            />
+            <Space h="md" />
+            <Group position="center" mt="xl">
+              <Button type="submit" size="xl" color="violet" compact>
+                Change email
+              </Button>
+            </Group>
+          </form>
+        </Box>
+      </Modal>
+      {
+        // password modal
+      }
+      <Modal
+        opened={passwordModalOpened}
+        onClose={() => setPasswordModalOpened(false)}
+      >
+        <Box sx={{ maxWidth: 340 }} mx="auto">
+          <form
+            onSubmit={passwordForm.onSubmit((values) => {
+              handleChangePassword({ ...values, userId: auth?.userInfo?.id });
+              setPasswordModalOpened(false);
+            })}
+          >
+            <PasswordInput
+              label="Password"
+              formProps={passwordForm.getInputProps('password')}
+            />
+            <Space h="lg" />
+            <PasswordInput
+              label="New Password"
+              formProps={passwordForm.getInputProps('newPassword')}
+            />
+            <Space h="lg" />
+            <PasswordInput
+              label="Confirm Password"
+              formProps={passwordForm.getInputProps('passwordConfirmation')}
+            />
+            <Space h="md" />
+            <Group position="center" mt="xl">
+              <Button type="submit" size="xl" color="violet" compact>
+                Change password
+              </Button>
+            </Group>
+          </form>
+        </Box>
+      </Modal>
     </Header>
   );
 };
